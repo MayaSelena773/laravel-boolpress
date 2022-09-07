5978f40a4f55a155a54ed0f0137dfb08afe2baa6
MayaSelena773 <maya.bono721@gmail.com>
2022-09-07 16:17:32 +0200

    link css and script

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.dashboard')
 
 @section('content')
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
+
 <div id="root">
     <div class="container">
         <h1>Sono la home pubblica! ^.^</h1>
     </div>
 </div>
+
+<script src="{{asset('js/app.js')}}"></script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,5 @@ Route::middleware('auth')
 
 Route::get('{any?}', function () {
     return view('guests.home');
-});
+})->where('any', '.*');
 
