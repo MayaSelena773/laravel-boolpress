ffedeb7c2e2b8011715faff219a9e22ab69ca109
MayaSelena773 <maya.bono721@gmail.com>
2022-09-07 17:03:17 +0200

    compilazione files

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
 <div class="container">
 <h1>Ciao sono la dashboard privata ;-)</h1> 
+
+<div>Benvenuto {{ $user->name}}</div>
+<div>La tua email è {{ $user->email}}</div>
 </div>
 @endsection
--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -1,7 +1,14 @@
-@extends('layouts.dashboard')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
-@section('content')
-<link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
 
 <div id="root">
     <div class="container">
@@ -10,4 +17,5 @@
 </div>
 
 <script src="{{asset('js/app.js')}}"></script>
-@endsection
+</body>
+</html>
