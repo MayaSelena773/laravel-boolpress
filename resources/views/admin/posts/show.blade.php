b8f0fa5aaffceb7cd45cb64f50c9cc55f0f651fe
MayaSelena773 <maya.bono721@gmail.com>
2022-09-08 19:29:17 +0200

    date with carbon

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,8 +4,8 @@
 
 <h1>{{ $post->title }}</h1>
 
-<p><strong>Creato il:</strong> {{ $post->created_at }}</p>
-<p><strong>Aggiornato il:</strong> {{ $post->updated_at }}</p>
+<p><strong>Creato il:</strong> {{ $post->created_at->format('j F Y') }}</p>
+<p><strong>Aggiornato il:</strong> {{ $post->updated_at->format('j F Y') }}</p>
 <p><strong>Slug:</strong> {{ $post->slug }}</p>
 
 <h3 class="mt-4">Contenuto:</h3>
