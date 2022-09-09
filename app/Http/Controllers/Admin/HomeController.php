dc31dfd502b31cf1bae6cfca60c3ecb0ded144c5
MayaSelena773 <maya.bono721@gmail.com>
2022-09-09 17:56:19 +0200

    relationship 1 to 1

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index() {
-        $user_info = UserInfo::find();
 
         $user = Auth::user();
 
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container">
-<h1>Ciao sono la dashboard privata ;-)</h1> 
+    <div class="container">
+        <h1>Ciao sono la dashboard privata ;-)</h1> 
 
-<div>Benvenuto {{ $user->name}}</div>
-<div>La tua email è {{ $user->email}}</div>
-</div>
+        <div>Benvenuto {{ $user->name}}</div>
+
+        <div>La tua email è {{ $user->email}}</div>
+
+        @if ($user->userInfo)
+
+            <div>Numero di telefono: {{ $user->userInfo->telephone }}</div>
+            
+            <div>Indirizzo: {{ $user->userInfo->address }}</div>
+            
+        @endif
+    </div>
 @endsection
