1f1296e69eb8acf664eb157286361a0987dd5da8
MayaSelena773 <maya.bono721@gmail.com>
2022-09-12 17:05:17 +0200

    correzione

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -28,7 +28,7 @@
         <select class="form-select" id="category_id" name="category_id">
             <option value="">Nessuna</option>
             @foreach ($categories as $category)
-            <option value="{{ $category->id }}" {{ old('category_id', $post->category->id) == $post->category->id ?? 'selected' : '' }}>{{ $category->name }}</option> 
+            <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>{{ $category->name }}</option> 
             @endforeach
         </select>
     </div>
